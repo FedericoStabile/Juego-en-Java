6480833bca0e9c3d644f591e5af3a4f0d96c5aed
LeoAristarco <chucky_92@hotmail.com.ar>
2016-06-01 10:38:29 -0300

    st_estasEnLaTabla_Label

--- tp_algoritmos.sql
+++ tp_algoritmos.sql
@@ -55,3 +55,28 @@ create table LABEL_ALBUM
 	FOREIGN KEY (la_label)      references LABEL(id_label) 
 	
 )
+
+
+ --si esta en la tabla de Label te devuelve  @bool =1 y @id_label el id corespondiente, si no esta @bool = 0 y @id_label es basura
+ create procedure st_estasEnLaTabla_Label
+ @nombre nvarchar(255),
+ @bool bit out,
+ @id_label numeric(10,0) out
+as
+begin
+declare @cant int
+
+      select @cant= count(*),@id_label = id_label
+	  from LABEL
+	  where label_nombre =@nombre
+	  group by id_label
+
+	  if (@cant <> 0)
+	   SET @bool=1
+      ELSE
+	   SET @bool=0
+
+	   print @bool
+	   print @id_label
+
+end
